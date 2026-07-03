97e9bacc8bb7bc80a55e37293c8e527737df3af6
haongLuan09 <hoangducluan258@gmail.com>
2026-07-04 02:14:12 +0700

    Use writable Vercel storage path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,32 @@
 <?php
 
+$runtimeStorage = '/tmp/laravel-storage';
+
+foreach ([
+    $runtimeStorage.'/app',
+    $runtimeStorage.'/app/travel_data',
+    $runtimeStorage.'/framework/cache/data',
+    $runtimeStorage.'/framework/sessions',
+    $runtimeStorage.'/framework/testing',
+    $runtimeStorage.'/framework/views',
+    $runtimeStorage.'/logs',
+] as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
+$sourceData = __DIR__.'/../storage/app/travel_data';
+$targetData = $runtimeStorage.'/app/travel_data';
+if (is_dir($sourceData)) {
+    foreach (glob($sourceData.'/*') ?: [] as $sourceFile) {
+        if (is_file($sourceFile)) {
+            copy($sourceFile, $targetData.'/'.basename($sourceFile));
+        }
+    }
+}
+
+$_ENV['LARAVEL_STORAGE_PATH'] = $runtimeStorage;
+$_SERVER['LARAVEL_STORAGE_PATH'] = $runtimeStorage;
+
 require __DIR__.'/../public/index.php';
